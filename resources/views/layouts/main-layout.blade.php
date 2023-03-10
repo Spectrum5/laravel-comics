fc5f8ef1abcd47d213be7f4c36816f4bedcd0bff
Spectrum5 <toskusergio@gmail.com>
2023-03-10 02:03:35 +0100

    Aggiornato header

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -6,9 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     {{-- Fontawesome --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
-        integrity="sha512-UaEi+kdJgKwLD6+mZSYFzR+KjZ+zGz25U6N7U6l2l6o3U6eKjxpWVGBaOQOQoLrkq3hX5KXOvfm5R5MVVr+Adw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
 
     <title>Laravel Comics | @yield('page-title')</title>
 
@@ -16,19 +14,17 @@
 </head>
 
 <body>
-    <div class="container">
-        <header>
-            @include('partials.header')
-        </header>
+    <header>
+        @include('partials.header')
+    </header>
 
-        <main>
-            @yield('main-content')
-        </main>
+    <main>
+        @yield('main-content')
+    </main>
 
-        <footer>
-            @include('partials.footer')
-        </footer>
-    </div>
+    <footer>
+        @include('partials.footer')
+    </footer>
 </body>
 
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,44 +1,54 @@
-<div class="container d-flex justify-content-between align-items-center">
-    <div class="logo-header">
-        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
-    </div>
-    <div class="nav-header d-flex align-items-center">
-        <ul class="d-flex">
-            <li>
-                <a href="#">characters</a>
-            </li>
-            <li>
-                <a href="#">comics</a>
-            </li>
-            <li>
-                <a href="#">movies</a>
-            </li>
-            <li>
-                <a href="#">tv</a>
-            </li>
-            <li>
-                <a href="#">games</a>
-            </li>
-            <li>
-                <a href="#">collectibles</a>
-            </li>
-            <li>
-                <a href="#">videos</a>
-            </li>
-            <li>
-                <a href="#">fans</a>
-            </li>
-            <li>
-                <a href="#">news</a>
-            </li>
-            <li>
-                <a href="#">shop</a>
-            </li>
-        </ul>
-    </div>
-    <div class="search">
-        <input type="text" placeholder=" Search &#xf002;"
-            class="border-0 border-bottom border-primary text-end fa fa-search">
-    </div>
+<div class="container">
+    <div class="row d-flex align-items-center">
 
+        <div class="col-2">
+            <div class="logo-header">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            </div>
+        </div>
+
+        <div class="col-8">
+            <div class="nav-header d-flex">
+                <ul class="d-flex">
+                    <li>
+                        <a href="#">characters</a>
+                    </li>
+                    <li>
+                        <a href="#">comics</a>
+                    </li>
+                    <li>
+                        <a href="#">movies</a>
+                    </li>
+                    <li>
+                        <a href="#">tv</a>
+                    </li>
+                    <li>
+                        <a href="#">games</a>
+                    </li>
+                    <li>
+                        <a href="#">collectibles</a>
+                    </li>
+                    <li>
+                        <a href="#">videos</a>
+                    </li>
+                    <li>
+                        <a href="#">fans</a>
+                    </li>
+                    <li>
+                        <a href="#">news</a>
+                    </li>
+                    <li>
+                        <a href="#">shop</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-2">
+            <div class="search">
+                <input type="text" placeholder=" Search &#xf002;"
+                    class="border-0 border-bottom border-primary text-end fa fa-search lowercase">
+            </div>
+        </div>
+    </div>
 </div>
